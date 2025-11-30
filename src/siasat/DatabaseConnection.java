7369623015026262b9624955bdd7edb2e3dc41a4
Qimberly07 <natasyanavaaureljutalo@gmail.com>
2025-12-01 04:39:56 +0700

    Add files via upload

---
+++ src/siasat/DatabaseConnection.java
@@ -0,0 +1,38 @@
+package Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_SistemSiasat";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+    
+    private static Connection connection;
+    
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+                System.out.println("Koneksi database db_SistemSiasat berhasil!");
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+                System.out.println("Koneksi database gagal!");
+            }
+        }
+        return connection;
+    }
+    
+    public static void closeConnection() {
+        if (connection != null) {
+            try {
+                connection.close();
+                connection = null;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
